2a775f31d015bdc76d8dc2d3fe1e7f0f1413a3d9
Daku-dad <102019367+Daku-dad@users.noreply.github.com>
2024-08-30 08:23:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,29 +16,29 @@
     <link
       rel="apple-touch-icon"
       sizes="180x180"
-      href="assets/img/favicons/apple-touch-icon.png"
+      href="/img/favicons/apple-touch-icon.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="32x32"
-      href="assets/img/favicons/favicon-32x32.png"
+      href="/img/favicons/favicon-32x32.png"
     />
     <link
       rel="icon"
-      type="image/png"
+      type="/image/png"
       sizes="16x16"
-      href="assets/img/favicons/favicon-16x16.png"
+      href="/img/favicons/favicon-16x16.png"
     />
     <link
       rel="shortcut icon"
       type="image/x-icon"
-      href="assets/img/favicons/favicon.ico"
+      href="/img/favicons/favicon.ico"
     />
-    <link rel="manifest" href="assets/img/favicons/manifest.json" />
+    <link rel="manifest" href="/img/favicons/manifest.json" />
     <meta
       name="msapplication-TileImage"
-      content="/assets/img/favicons/mstile-150x150.png"
+      content="/img/favicons/mstile-150x150.png"
     />
     <meta name="theme-color" content="#ffffff" />
 
